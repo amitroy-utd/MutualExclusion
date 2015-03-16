260462375a0a58077465a6ad033636a0a18c94bc
Amit Roy <amitr@visualbis.com>
2015-03-15 21:43:52 -0500

    Algorithm implementation
    -Global Map holding info about all nodes
    -Key Map
    -cs_enter

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -2,7 +2,9 @@ import java.io.*;
 import java.util.*;
 public class Algorithm {
 	public static Map<Integer, String> map=Collections.synchronizedMap(new TreeMap<Integer, String>());	
-	public static Map<String, String> cs_queue=Collections.synchronizedMap(new TreeMap<String, String>());	
+	public static Map<String, String> cs_queue=Collections.synchronizedMap(new TreeMap<String, String>());
+	public static Map<Integer, String> shared_keys=Collections.synchronizedMap(new TreeMap<Integer, String>());
+	
 	public static int NodeID=0;
 	public void getNodeInfoFromFile(int nodeId, String topologyFile)
 	{
@@ -15,7 +17,26 @@ public class Algorithm {
 			while((line=br.readLine())!=null)
 			{
 				String data[] = line.split(" ");
-				map.put(Integer.parseInt(data[0]),data[1]);						
+				map.put(Integer.parseInt(data[0]),data[1]);	
+				if(data.length==6)
+				{
+					if(nodeId==Integer.parseInt(data[0]));
+					{
+						String []key_arr=data[5].split(":");
+						for(int i=0;i<key_arr.length;i++)
+						{
+							String []keys=key_arr[i].split(",");
+							if(Integer.parseInt(keys[0])!=nodeId && Integer.parseInt(keys[1])==nodeId)
+							{
+								shared_keys.put(Integer.parseInt(keys[0]), key_arr[i]);
+							}
+							else if(Integer.parseInt(keys[1])!=nodeId && Integer.parseInt(keys[0])==nodeId)
+							{
+								shared_keys.put(Integer.parseInt(keys[1]), key_arr[i]);
+							}							
+						}						
+					}					
+				}				
 			}
 			br.close();
 			for (Map.Entry<Integer, String> entry : map.entrySet())
@@ -37,13 +58,32 @@ public class Algorithm {
 	public static void main(String []args)
 	{
 		Algorithm al=new Algorithm();
-		al.getNodeInfoFromFile(1, "topology.txt");
-		System.out.println("=========>"+getInfoFromMap(2));
+		al.getNodeInfoFromFile(1, "topology.txt");		
+	}
+	public static boolean checkKeys()
+	{
+		if(shared_keys.size()==(map.size()-1))
+		{
+			return true;
+		}
+		return false;
+	}
+	public static void cs_handler()
+	{
+		if(checkKeys()==true)
+		{
+			// execute critical section
+		}
+		else
+		{
+			// request for keys
+		}
 	}
 	public static void cs_enter()
 	{
-		 long timestamp=System.currentTimeMillis();
-		 cs_queue.put(timestamp+"-"+NodeID,Integer.toString(NodeID)); //""+NodeID+""
+		long timestamp=System.currentTimeMillis();		
+		cs_queue.put(timestamp+"_"+NodeID,Integer.toString(NodeID)); //""+NodeID+""
+		cs_handler();
 		 		 
 	}
 }
